3ecb70ef7ffc94030fb27ef83c368fbef83f19a9
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2026-05-27 13:55:16 +0200

    INITIALISED BACKEND-API

---
+++ src/api/app/main.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan function, used to do stuff before or after server is online"""
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"])
