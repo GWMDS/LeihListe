67fde8bab5e175ec92feaa50be29dfe1524a236d
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2026-06-02 09:36:13 +0200

    chore(database): changed database configuration + example.env

--- src/api/app/database.py
+++ src/api/app/database.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url = f"postgresql+psycopg2://{os.environ['DB_USER']}:{os.environ['DB_PASS']}@{os.environ['DB_ADDR']}/learndb"
+db_url = f"postgresql+psycopg2://{os.environ['DB_USER']}:{os.environ['DB_PASS']}@{os.environ['DB_ADDR']}/lendify"
 
 engine = create_engine(db_url)
