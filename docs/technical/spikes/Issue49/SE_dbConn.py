407c7666b2a801ee8ca63f1813414a36f84f5a64
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2026-06-08 21:02:24 +0200

    chore(script): Added scripts

---
+++ docs/technical/spikes/Issue49/SE_dbConn.py
@@ -0,0 +1,11 @@
+import pyodbc
+
+
+"""stellt nur Verbindung mit DBS her"""
+"""Database, uid und pwd mit s-nummer ersetzen"""
+def getConn ():
+    cnxn = pyodbc.connect("Driver={sql server};"
+                          "Server=141.56.2.46;"
+                          "Database=ixxxs12345;"	# Bsp.: ii24s12345 (ii...allg. Informatik; 24...Immatrikulationsjahr)
+                          "uid=s12345;pwd=s12345")
+    return cnxn
