f0ab2a734fb1ba29c5a2fe61ceff4d93d452fb7f
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-06-10 21:20:38 +0200

    chore(api): rename Item class to upper case

--- src/api/app/models.py
+++ src/api/app/models.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field
 
-class item(SQLModel, table=True):
+class Item(SQLModel, table=True):
     """
     This is an item that is stored in the database.
     The items can be borrowed.
