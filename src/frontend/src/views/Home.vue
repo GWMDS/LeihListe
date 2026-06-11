692ba30f7d711e2b3b814bb550d79946b4255815
franzborrmann <franz.borrmann@outlook.com>
2026-06-11 14:53:51 +0200

    feat(frontend): add API test button and textarea for items fetching

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -3,18 +3,31 @@
   <v-btn color="primary" @click="Test">
     API GET Test
   </v-btn>
+
+  <v-textarea
+      v-model="itemsText"
+      label="Backend Items"
+      readonly
+      rows="10"
+      class="mt-4"
+      variant="outlined"
+  ></v-textarea>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import api from '../api.ts'
 
+const itemsText = ref('')
 async function Test() {
   try {
-    const response = await api.get('/')
+    const response = await api.get('/api/items')
+    itemsText.value =JSON.stringify(response.data,null,2)
     console.log(response.data)
   } catch (error: any) {
-    console.error("Status: " + error.response.status)
-    console.error("Data Detail: " + error.response.data.detail)
+      console.error("Status: " + error.response.status)
+      console.error("Data Detail: " + error.response?.data.detail)
+      itemsText.value = "Fehler-Status: " + error.response.status
   }
 }
 
