2b5f25c8dc4b75050af71f9152db9ca38d4a9d31
chrisvineyard52-design <chrisvineyard52@gmail.com>
2026-06-27 11:46:26 +0200

    chore(issue63): für eine Person Ausleihe anzeigen

--- src/api/app/routers/borrowings.py
+++ src/api/app/routers/borrowings.py
@@ -16,3 +16,14 @@ def get_borrowings_with_details(session: Session = Depends(get_session))-> list[
 
     return borrowings
 
+
+# issue 63 - get all borrowings with details for one person
+@router.get("/all/details/{customer_id}")#, response_model=Borrowing)
+def get_borrowings_with_details_for_customer(customer_id: int, session: Session = Depends(get_session))-> list[Borrowing]:
+    """Returns all borrowings for a specific customer."""
+    borrowings = session.exec(select(Borrowing).where(Borrowing.customer_id == customer_id)).all()
+
+    if  not customer_id in [borrowing.customer_id for borrowing in borrowings]:
+        raise HTTPException(status_code=404, detail="no borrowings for customer found")
+
+    return borrowings
